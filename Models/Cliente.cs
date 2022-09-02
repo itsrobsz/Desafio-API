df42c3bf4b37e3a4d4505931c516b86d339de2c4
Roberta Oliveira <102633229+itsrobsz@users.noreply.github.com>
2022-09-02 08:51:53 -0300

    Models

---
+++ Models/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APISistemaVeterinario.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        // [Required] = Valida que a propriedade tenha um valor
+        [Required(ErrorMessage = "Informe o nome do cliente.")]
+        // Define um tamanho máximo a ser digitado
+        [MaxLength(200)]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "Informe um endereço.")]
+        // Define um tamanho máximo a ser digitado
+        [MaxLength(200)]
+        public string Endereco { get; set; }
+
+
+        [Required(ErrorMessage = "Informe um email.")]
+        // Define um tamanho máximo a ser digitado
+        [MaxLength(160)]
+        // Valida o formato do email 
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Favor informar um email válido.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Informe um número de telefone para contato.")]
+        public int Telefone { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o CPF.")]
+        // Define um tamanho mínimo a ser digitado
+        [MinLength(11)]
+        // Define um tamanho máximo a ser digitado
+        [MaxLength(11)]
+        public string CPF { get; set; }
+
+        public string Imagem { get; set; }
+    }
+}
---
+++ Models/Consulta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APISistemaVeterinario.Models
+{
+    public class Consulta
+    {
+        // [Required] = Valida que a propriedade tenha um valor
+        public int Id { get; set; }
+
+        public DateTime DataHora { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o valor da consulta.")]
+        public float Valor { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o Id do veterinário.")]
+        public int VeterinarioId { get; set; }
+        public Veterinario Veterinario { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o Id do animal.")]
+        public int AnimalId { get; set; }
+
+        // Faz a associação da chave estrangeira
+        public Animal Animal { get; set; }
+    }
+}
